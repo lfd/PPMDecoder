7e876175567cabade18550343a5024454ccdc795
Tobias Kottwitz <tobias.kottwitz@st.oth-regensburg.de>
2016-12-12 16:14:18 +0100

    add main loop

--- main.c
+++ main.c
@@ -5,12 +5,33 @@
  *
  * Authors:
  *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *  Tobias Kottwitz <tobias.kottwitz@oth-regensburg.de>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
  */
 
 int main(void) {
-	for(;;);
-	return 0;
+	char buffer[128];
+	int i;
+
+	cli();
+
+	uart_init();
+	uart_puts("Hello, world!\r\n");
+
+	ppm_init();
+	sei();
+
+	for(;;) {
+
+		/* dummy code */
+		for (i=0;i<CHANNELS;i++) {
+			sprintf(buffer, "%u ", *(ppm_data+i));
+			uart_puts(buffer);
+		}
+		uart_puts("\n\r");
+
+		_delay_ms(100);
+   	}
 }
